b0a20b76963b3703d23e24d33b73ad20419a4b29
Warren Simpson <warren.simpson34@gmail.com>
2021-12-12 12:03:58 -0500

    Updated the CaseStudies cell to change the opacity when hovering. To accomplish this, The LandingPageContainer was updated with new functions and variables in the state.

--- src/components/containers/LandingPageContainer.js
+++ src/components/containers/LandingPageContainer.js
@@ -5,19 +5,47 @@ class LandingPageContainer extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-
+      case1Opacity: "1",
+      case2Opacity: "1",
     };
   }
 
-
   handleClick = (e) => {
 
+  }
+
+  handleCaseOverEnter = (e, caseNumber) => {
+    if(caseNumber === "1") {
+      this.setState({
+        case1Opacity: "0.7"
+      })
+    }
+    else {
+      this.setState({
+        case2Opacity: "0.7"
+      })
+    }
+  }
+
+  handleCaseOverLeave = (e, caseNumber) => {
+    if(caseNumber === "1") {
+      this.setState({
+        case1Opacity: "1"
+      })
+    }
+    else {
+      this.setState({
+        case2Opacity: "1"
+      })
+    }
   }
 
   render() {
     return (
       <>
       <LandingPageView
+      handleCaseOverEnter={this.handleCaseOverEnter}
+      handleCaseOverLeave={this.handleCaseOverLeave}
       handleClick={this.handleClick}
       state={this.state}
       />
